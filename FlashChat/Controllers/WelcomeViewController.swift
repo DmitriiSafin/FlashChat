9a0a06fd21c718eb7db744d0fc0dac12e4c29c40
Dmitry Safin <developersafin@gmail.com>
2023-03-13 18:54:17 +0500

    Control point

---
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  FlashChat
+//
+//  Created by Дмитрий on 13.03.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
+
+
+}
+
