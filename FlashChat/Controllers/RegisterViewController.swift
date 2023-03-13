9b7e39f6bd5d8999bf2a16226c437ba407c41247
Dmitry Safin <developersafin@gmail.com>
2023-03-13 20:52:36 +0500

    Added Alamofire

--- FlashChat/Controllers/RegisterViewController.swift
+++ FlashChat/Controllers/RegisterViewController.swift
@@ -6,29 +6,38 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
 
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    self.alertRegister(title: "Attention", message: error.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func alertRegister(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
-    */
 
 }
--- FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/Controllers/WelcomeViewController.swift
@@ -13,7 +13,7 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         titleLabel.text = ""
         var charIndex = 0.0
         let titleText = "⚡️FlashChat"
